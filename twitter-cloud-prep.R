8f776e270ce47144c6833dd14b18c40717a61b58
Peter Ellis <peter.ellis2013nz@gmail.com>
2018-05-20 07:50:49 +0000

    a bit of basic analysis and monitoring

--- twitter-cloud-prep.R
+++ twitter-cloud-prep.R
@@ -3,7 +3,7 @@ library(RPostgres)
 
 con <- dbConnect(RPostgres::Postgres(), dbname = "twitter")
 
-# dbGetQuery(con, "select * from tweets.batches order by batch_id") %>% View
+dbGetQuery(con, "select * from tweets.batches order by batch_id") %>% View
 
 
 sql <-"
