85bd451d98a220cbd42d651f2b4245b7fb22aac5
Peter Ellis <peter.ellis2013nz@gmail.com>
2018-07-04 21:34:39 +0000

    css for fonts

--- twitter-monitor/ui.R
+++ twitter-monitor/ui.R
@@ -5,6 +5,12 @@ load("top_ten_lang.rda")
 
 
 shinyUI(fluidPage(
+  tags$style(HTML("@import url('https://fonts.googleapis.com/css?family=Roboto');
+@import url('https://fonts.googleapis.com/css?family=Prosto One');
+  ")),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "my_styles.css")
+  ),
   
   # Application title
   titlePanel("A representative random sample of tweets"),
@@ -41,7 +47,14 @@ shinyUI(fluidPage(
             imageOutput("tweeters")   
                  ),
         tabPanel("Sampling",
-            imageOutput("batches")
+            imageOutput("batches"),
+            p("The image above shows the rate at which new data is being 
+              sampled from Twitter into the database, in 30 second bursts.
+              For example, around 1,000 to 3,000 tweets are loaded in each 
+              burst.  Sampling bursts take place once an hour at a random
+              time."),
+            p("The number of tweets show both daily and weekly periodicity;
+              peak time is around 15:30 UTC each day.")
             )
       )
     )
